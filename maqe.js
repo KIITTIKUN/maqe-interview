d9908f5e281eac6266ec25aed1c03752f969a1cf
Akaradat Kawmaneechot <akrd.kmnc@gmail.com>
2022-06-23 11:34:34 +0700

    fix(bot): remove unused command

--- maqe.js
+++ maqe.js
@@ -1,18 +1,16 @@
-
-[0,1,2,3]
 const direction = {
-    NORTH : 0,
-    EAST : 1,
-    SOUTH : 2,
-    WEST : 3,
-} 
-// DRY = Don't repeat yourself 
+  NORTH: 0,
+  EAST: 1,
+  SOUTH: 2,
+  WEST: 3,
+};
+// DRY = Don't repeat yourself
 const directionStr = {
-    NORTH : 0,
-    EAST : 1,
-    SOUTH : "South",
-    WEST : 3,
-} 
+  NORTH: 0,
+  EAST: 1,
+  SOUTH: 'South',
+  WEST: 3,
+};
 
 /* 
 1. Verbose
@@ -24,100 +22,122 @@ const directionStr = {
 */
 
 const assignMaqeBotByValue = (value, index) => {
-    if(maqeBot[index] === ''){
-        maqeBot.splice(index, 1)
-    } else {
-        maqeBot.splice(index,0);
-    }
-}
+  if (maqeBot[index] === '') {
+    maqeBot.splice(index, 1);
+  } else {
+    maqeBot.splice(index, 0);
+  }
+};
 
 // maqe('RRRLLLLRRRRR') // ''
 // maqe('LLLRRRRLLLLL') // ''
 
+function maqe(control) {
+  //assign valiables degree, x , y
+  let deg = 0,
+    x = 0,
+    y = 0,
+    direction = '';
 
-function maqe(control){
-    //assign valiables degree, x , y
-    let deg = 0, x = 0, y = 0, direction = '';
+  const matchQuadraL = /LLLL/g;
+  const matchEmail = /\S+@\w+.com/g;
 
-    const matchQuadraL = /LLLL/g
-    const matchEmail = /\S+@\w+.com/g
-    
-    //preparewastecontrol By Regular Expression
-    let prepareControl  = control.replace(matchQuadraL,'').replace(/RRRR/g,'').replace(/RL/g,'').replace(/LR/g,''); 
+  //preparewastecontrol By Regular Expression
+  let prepareControl = control
+    .replace(matchQuadraL, '')
+    .replace(/RRRR/g, '')
+    .replace(/RL/g, '')
+    .replace(/LR/g, '');
 
-    //create array for maqeBot
-    const maqeBot = prepareControl.split(/(\D)/);
-// console.log(maqeBot)
-    //filter 
-    for (let index in maqeBot){
-        maqeBot[index] === '' ? maqeBot.splice(index,1): maqeBot.splice(index,0);
-        // maqeBot[undefined]
+  //create array for maqeBot
+  const maqeBot = prepareControl.split(/(\D)/);
+  // console.log(maqeBot)
+  //filter
+  for (let index in maqeBot) {
+    maqeBot[index] === '' ? maqeBot.splice(index, 1) : maqeBot.splice(index, 0);
+    // maqeBot[undefined]
+  }
+  // console.log(maqeBot)
+
+  //process maqeBot
+  // maqeBot[index] = currentDirection
+  for (let index = 0; index < maqeBot.length; index++) {
+    let currentDirection = maqeBot[index];
+    if (currentDirection === 'R') {
+      //Turn Right
+      deg += 90;
+    } else if (currentDirection === 'L') {
+      //Turn Left
+      deg -= 90;
+    } else if (currentDirection === 'W') {
+      //Go straigth
+      index++;
+      currentDirection = maqeBot[index];
+      if (deg % 360 === 90 || deg % 360 === -270) {
+        x += Number(currentDirection);
+        direction = 'East';
+      } else if (deg % 360 === 180 || deg % 360 === -180) {
+        y -= Number(currentDirection);
+        direction = 'South';
+      } else if (deg % 360 === 270 || deg % 360 === -90) {
+        x -= Number(currentDirection);
+        direction = 'West';
+      } else {
+        y += Number(currentDirection);
+        direction = 'North';
+      }
     }
-// console.log(maqeBot)
-    
+  }
 
-    //process maqeBot
-    // maqeBot[index] = currentDirection
-    for(let index = 0; index < maqeBot.length; index++){
-        let currentDirection = maqeBot[index]
-        if(currentDirection === 'R'){ 
-        //Turn Right
-            deg += 90;
-        } else if(currentDirection === 'L'){ 
-        //Turn Left
-            deg -= 90;
-        } else if(currentDirection === 'W'){ 
-        //Go straigth
-            index++;
-            currentDirection = maqeBot[index]
-            if(deg % 360 === 90 || deg % 360 === -270){  
-                x += Number(currentDirection);
-                direction = 'East';
-            } else if(deg % 360 === 180 || deg % 360 === -180){
-                y -= Number(currentDirection);
-                direction = 'South';
-            } else if(deg % 360 === 270 || deg % 360 === -90){
-                x -= Number(currentDirection);
-                direction = 'West';
-            } else {
-                y += Number(currentDirection);
-                direction = 'North';
-            }
-        }
-    }  
-
-
-    return {
-        x : x,
-        y : y,
-        direction: direction
-    };
+  return {
+    x: x,
+    y: y,
+    direction: direction,
+  };
 }
 
-
-function choiceTest(arr,choice){
-    let isValid = false, testLocate = arr[choice];
-    switch(choice){
-        case 0: 
-            isValid = testLocate.x === 15 && testLocate.y === -1 && testLocate.direction === 'South' ;
-            break;
-        case 1: 
-            isValid = testLocate.x === 4 && testLocate.y === 3 && testLocate.direction === 'West' ;
-            break; 
-        case 2: 
-            isValid = testLocate.x === 7 && testLocate.y === -12 && testLocate.direction === 'South' ;
-            break;
-        case 3: 
-            isValid = testLocate.x === -210 && testLocate.y === -150 && testLocate.direction === 'West' ;
-            break;
-        case 4: 
-            isValid = testLocate.x === -99 && testLocate.y === 88 && testLocate.direction === 'North' ;
-            break; 
-        case 5: 
-            isValid = testLocate.x === 1000000 && testLocate.y === 55555 && testLocate.direction === 'East' ;
-            break;
-    }
-    return isValid;     
+function choiceTest(arr, choice) {
+  let isValid = false,
+    testLocate = arr[choice];
+  switch (choice) {
+    case 0:
+      isValid =
+        testLocate.x === 15 &&
+        testLocate.y === -1 &&
+        testLocate.direction === 'South';
+      break;
+    case 1:
+      isValid =
+        testLocate.x === 4 &&
+        testLocate.y === 3 &&
+        testLocate.direction === 'West';
+      break;
+    case 2:
+      isValid =
+        testLocate.x === 7 &&
+        testLocate.y === -12 &&
+        testLocate.direction === 'South';
+      break;
+    case 3:
+      isValid =
+        testLocate.x === -210 &&
+        testLocate.y === -150 &&
+        testLocate.direction === 'West';
+      break;
+    case 4:
+      isValid =
+        testLocate.x === -99 &&
+        testLocate.y === 88 &&
+        testLocate.direction === 'North';
+      break;
+    case 5:
+      isValid =
+        testLocate.x === 1000000 &&
+        testLocate.y === 55555 &&
+        testLocate.direction === 'East';
+      break;
+  }
+  return isValid;
 }
 
 // const location = [maqe('RW15RW1'), maqe('W5RW5RW2RW1R'), maqe('RRW11RLLW19RRW12LW1'), maqe('LLW100W50RW200W10'), maqe('LLLLLW99RRRRRW88LLLRL'), maqe('W55555RW555555W444444W1')];
@@ -128,7 +148,4 @@ function choiceTest(arr,choice){
 // console.log(`5: at location X:${location[4].x}, Y:${location[4].y}, direction: ${location[4].direction},\t\t validate: ${choiceTest(location,4)? 'valid':'invalid'}`) // actual {x: -99 ,y:88} expected {x:-99 ,y:y:88} (valid)
 // console.log(`6: at location X:${location[5].x}, Y:${location[5].y},\ direction: ${location[5].direction},\t\t validate: ${choiceTest(location,5)? 'valid':'invalid'}`) // actual {x: 1000000 ,y:55555} expected {x: 1000000 ,y:-55555} (valid)
 
-console.log(maqe('RW15RW1R'))
-
-
-
+console.log(maqe('RW15RW1R'));
