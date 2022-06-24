d2e6e3d51f82f0e0225a67aa49d8b1a3d2ca475c
KIITTIKUN <kittikun.real@gmail.com>
2022-06-24 15:38:56 +0700

    Update maqe.js

--- maqe.js
+++ maqe.js
@@ -11,7 +11,15 @@ const matchUnnecessary = {
   controlRL: /RL/g,
   controlLR: /LR/g,
 };
+const buildArrayForBot = (string) => {
+  return string.split(/(\D)/);
+};
 
+const cutNullString = (controlString, index, arrayBot) => {
+  return controlString === ''
+    ? arrayBot[index].splice(index, 1)
+    : arrayBot[index].splice(index, 0);
+};
 function maqeBot(control) {
   let degrees = 0,
     x = 0,
@@ -24,41 +32,37 @@ function maqeBot(control) {
     .replace(matchUnnecessary.controlRL, '')
     .replace(matchUnnecessary.controlLR, '');
 
-  const process = prepareControl.split(/(\D)/);
+  const process = buildArrayForBot(prepareControl);
 
-  process.forEach(function (control, index, process) {
-    control === '' ? process.splice(index, 1) : process.splice(index, 0);
-  });
+  process.forEach(cutNullString());
 
-  for (let index = 0; index < process.length; index++) {
-    let currentDirection = process[index];
-    if (currentDirection === 'R') {
-      degrees += 90;
-    } else if (currentDirection === 'L') {
-      degrees -= 90;
-    } else if (currentDirection === 'W') {
-      index++;
-      currentDirection = process[index];
-      if (degrees % 360 === 90 || degrees % 360 === -270) {
-        x += Number(currentDirection);
-        direction = 'East';
-      } else if (degrees % 360 === 180 || degrees % 360 === -180) {
-        y -= Number(currentDirection);
-        direction = 'South';
-      } else if (degrees % 360 === 270 || degrees % 360 === -90) {
-        x -= Number(currentDirection);
-        direction = 'West';
-      } else {
-        y += Number(currentDirection);
-        direction = 'North';
-      }
-    }
-  }
+  //   for (let index = 0; index < process.length; index++) {
+  //     let currentDirection = process[index];
+  //     if (currentDirection === 'R') {
+  //       degrees += 90;
+  //     } else if (currentDirection === 'L') {
+  //       degrees -= 90;
+  //     } else if (currentDirection === 'W') {
+  //       index++;
+  //       currentDirection = process[index];
+  //       if (degrees % 360 === 90 || degrees % 360 === -270) {
+  //         x += Number(currentDirection);
+  //         direction = 'East';
+  //       } else if (degrees % 360 === 180 || degrees % 360 === -180) {
+  //         y -= Number(currentDirection);
+  //         direction = 'South';
+  //       } else if (degrees % 360 === 270 || degrees % 360 === -90) {
+  //         x -= Number(currentDirection);
+  //         direction = 'West';
+  //       } else {
+  //         y += Number(currentDirection);
+  //         direction = 'North';
+  //       }
+  //     }
+  //   }
 
   return {
-    x: x,
-    y: y,
-    direction: direction,
+    process,
   };
 }
 
