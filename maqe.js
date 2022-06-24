29c244e92fdfdc82a459e92107db1a4b1bd578d7
KIITTIKUN <kittikun.real@gmail.com>
2022-06-24 15:04:42 +0700

    refactor(bot): change variables name

--- maqe.js
+++ maqe.js
@@ -1,54 +1,51 @@
-const direction = {
+const currentDirection = {
   0: 'NORTH',
   1: 'EAST',
   2: 'SOUTH',
   3: 'WEST',
 };
 
-const assignMaqeBotByValue = (value, index) => {
-  if (maqeBot[index] === '') {
-    maqeBot.splice(index, 1);
-  } else {
-    maqeBot.splice(index, 0);
-  }
+const matchUnnecessary = {
+  quadraL: /LLLL/g,
+  quadraR: /RRRR/g,
+  controlRL: /RL/g,
+  controlLR: /LR/g,
 };
 
-function maqe(control) {
-  let deg = 0,
+function maqeBot(control) {
+  let degrees = 0,
     x = 0,
     y = 0,
     direction = '';
 
-  const matchQuadraL = /LLLL/g;
-
   let prepareControl = control
-    .replace(matchQuadraL, '')
-    .replace(/RRRR/g, '')
-    .replace(/RL/g, '')
-    .replace(/LR/g, '');
+    .replace(matchUnnecessary.quadraL, '')
+    .replace(matchUnnecessary.quadraR, '')
+    .replace(matchUnnecessary.controlRL, '')
+    .replace(matchUnnecessary.controlLR, '');
 
-  const maqeBot = prepareControl.split(/(\D)/);
+  const process = prepareControl.split(/(\D)/);
 
-  for (let index in maqeBot) {
-    maqeBot[index] === '' ? maqeBot.splice(index, 1) : maqeBot.splice(index, 0);
-  }
+  process.forEach(function (control, index, process) {
+    control === '' ? process.splice(index, 1) : process.splice(index, 0);
+  });
 
-  for (let index = 0; index < maqeBot.length; index++) {
-    let currentDirection = maqeBot[index];
+  for (let index = 0; index < process.length; index++) {
+    let currentDirection = process[index];
     if (currentDirection === 'R') {
-      deg += 90;
+      degrees += 90;
     } else if (currentDirection === 'L') {
-      deg -= 90;
+      degrees -= 90;
     } else if (currentDirection === 'W') {
       index++;
-      currentDirection = maqeBot[index];
-      if (deg % 360 === 90 || deg % 360 === -270) {
+      currentDirection = process[index];
+      if (degrees % 360 === 90 || degrees % 360 === -270) {
         x += Number(currentDirection);
         direction = 'East';
-      } else if (deg % 360 === 180 || deg % 360 === -180) {
+      } else if (degrees % 360 === 180 || degrees % 360 === -180) {
         y -= Number(currentDirection);
         direction = 'South';
-      } else if (deg % 360 === 270 || deg % 360 === -90) {
+      } else if (degrees % 360 === 270 || degrees % 360 === -90) {
         x -= Number(currentDirection);
         direction = 'West';
       } else {
@@ -65,4 +62,4 @@ function maqe(control) {
   };
 }
 
-console.log(maqe('RW15RW1R'));
+console.log(maqeBot('RW15RW1R'));
