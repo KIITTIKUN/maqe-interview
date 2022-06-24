ec77b39e0191b3379f3d49943a3bd0efb74d55d5
KIITTIKUN <kittikun.real@gmail.com>
2022-06-25 00:24:11 +0700

    refactor(bot): using array.method instead normal loop

--- maqe.js
+++ maqe.js
@@ -22,6 +22,12 @@ const buildArrayForBot = (string) => {
   return arrayControlBot;
 };
 
+const currentControl = {
+  turnRight: 'R',
+  turnLeft: 'L',
+  goStraight: 'W',
+};
+
 function maqeBot(control) {
   let degrees = 0,
     x = 0,
@@ -36,30 +42,52 @@ function maqeBot(control) {
 
   const processBot = buildArrayForBot(prepareControl);
 
-  for (let index = 0; index < processBot.length; index++) {
-    let currentDirection = processBot[index];
-    if (currentDirection === 'R') {
+  processBot.forEach(function (control) {
+    if (control === currentControl.turnRight) {
       degrees += 90;
-    } else if (currentDirection === 'L') {
+    } else if (control === currentControl.turnLeft) {
       degrees -= 90;
-    } else if (currentDirection === 'W') {
-      index++;
-      currentDirection = processBot[index];
+    } else if (control === currentControl.goStraight) {
+    } else {
       if (degrees % 360 === 90 || degrees % 360 === -270) {
-        x += Number(currentDirection);
+        x += Number(control);
         direction = 'East';
       } else if (degrees % 360 === 180 || degrees % 360 === -180) {
-        y -= Number(currentDirection);
+        y -= Number(control);
         direction = 'South';
       } else if (degrees % 360 === 270 || degrees % 360 === -90) {
-        x -= Number(currentDirection);
+        x -= Number(control);
         direction = 'West';
       } else {
-        y += Number(currentDirection);
+        y += Number(control);
         direction = 'North';
       }
     }
-  }
+  });
+  //   for (let index = 0; index < processBot.length; index++) {
+  //     let currentDirection = processBot[index];
+  //     if (currentDirection === 'R') {
+  //       degrees += 90;
+  //     } else if (currentDirection === 'L') {
+  //       degrees -= 90;
+  //     } else if (currentDirection === 'W') {
+  //       continue;
+  //     } else {
+  //       if (degrees % 360 === 90 || degrees % 360 === -270) {
+  //         x += Number(currentDirection);
+  //         direction = 'East';
+  //       } else if (degrees % 360 === 180 || degrees % 360 === -180) {
+  //         y -= Number(currentDirection);
+  //         direction = 'South';
+  //       } else if (degrees % 360 === 270 || degrees % 360 === -90) {
+  //         x -= Number(currentDirection);
+  //         direction = 'West';
+  //       } else {
+  //         y += Number(currentDirection);
+  //         direction = 'North';
+  //       }
+  //     }
+  //   }
 
   return {
     x: x,
